3edefc208b7f0c0992eb63348cd314de23fd7748
shytuleva24 <shutyleva@gmail.com>
2023-03-24 03:54:38 +0200

    httml/css + adaptive not js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,6 +10,7 @@ window.addEventListener('load', () => {
     });
     
 });
+
 function menuMobile() {
     const iconMenu = document.querySelector('.menu-icon');
     const homeMenuLinks = document.querySelectorAll(".scroll-to");
@@ -24,50 +25,50 @@ function menuMobile() {
     
     }
     
-    if (homeMenuLinks.length > 0) {
-        homeMenuLinks.forEach(link => {
-            link.onclick = function (event) {
-                event.preventDefault();
-                onMenuLinkClick(this.getAttribute("href"));
-            }
-        });
+    // if (homeMenuLinks.length > 0) {
+    //     homeMenuLinks.forEach(link => {
+    //         link.onclick = function (event) {
+    //             event.preventDefault();
+    //             onMenuLinkClick(this.getAttribute("href"));
+    //         }
+    //     });
     
-        function onMenuLinkClick(href) {
-            if (document.querySelector(href)) {
-                const target = document.querySelector(href);
-                let targetLocation = target.getBoundingClientRect().top + window.pageYOffset;
+    //     function onMenuLinkClick(href) {
+    //         if (document.querySelector(href)) {
+    //             const target = document.querySelector(href);
+    //             let targetLocation = target.getBoundingClientRect().top + window.pageYOffset;
     
-                if (iconMenu.classList.contains("open-menu")) {
-                    document.body.classList.remove('lock');
-                    iconMenu.classList.remove("open-menu");
-                    menuBody.classList.remove("open-menu");
-                }
-                window.scrollTo({
-                    top: targetLocation,
-                    behavior: "smooth"
-                });
-            }
-        }
-    }
+    //             if (iconMenu.classList.contains("open-menu")) {
+    //                 document.body.classList.remove('lock');
+    //                 iconMenu.classList.remove("open-menu");
+    //                 menuBody.classList.remove("open-menu");
+    //             }
+    //             window.scrollTo({
+    //                 top: targetLocation,
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //     }
+    // }
     
 }
 
-function createScrollListener(elementSelector, visibleCallback, hiddenCallback, threshold) {
-    const element = elementSelector;
+// function createScrollListener(elementSelector, visibleCallback, hiddenCallback, threshold) {
+//     const element = elementSelector;
 
-    function handleIntersection(entries) {
-        if (entries[0].isIntersecting) {
-            visibleCallback();
-        } else {
-            hiddenCallback();
-        }
-    }
+//     function handleIntersection(entries) {
+//         if (entries[0].isIntersecting) {
+//             visibleCallback();
+//         } else {
+//             hiddenCallback();
+//         }
+//     }
 
-    const options = {
-        threshold: threshold || 0.5
-    };
+//     const options = {
+//         threshold: threshold || 0.5
+//     };
 
-    const observer = new IntersectionObserver(handleIntersection, options);
-    observer.observe(element);
-}
+//     const observer = new IntersectionObserver(handleIntersection, options);
+//     observer.observe(element);
+// }
 
