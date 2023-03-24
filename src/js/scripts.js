0b5f83482ec56c047e3c56466ee36c63b3b722f1
shytuleva24 <shutyleva@gmail.com>
2023-03-24 13:47:25 +0200

    smooth scroll, add border bottom header, pricing span price blue color, menu mobile font-size, footel add link

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,74 +1,135 @@
-window.addEventListener('load', () => {
-    menuMobile();
+// window.addEventListener('load', () => {
+//     menuMobile();
 
-    document.querySelectorAll('.animated').forEach(element => {
-        createScrollListener(element, function () {
-            element.classList.add(`fadeInDown`);
-        }, function () {
-            element.classList.remove(`fadeInDown`);
-        }, 0.3);
-    });
+//     document.querySelectorAll('.animated').forEach(element => {
+//         createScrollListener(element, function () {
+//             element.classList.add(`fadeInDown`);
+//         }, function () {
+//             element.classList.remove(`fadeInDown`);
+//         }, 0.3);
+//     });
     
-});
+// });
 
-function menuMobile() {
-    const iconMenu = document.querySelector('.menu-icon');
-    const homeMenuLinks = document.querySelectorAll(".scroll-to");
-    const menuBody = document.querySelector('.menu-body');
+// function menuMobile() {
+//     const iconMenu = document.querySelector('.menu-icon');
+//     const homeMenuLinks = document.querySelectorAll(".scroll-to");
+//     const menuBody = document.querySelector('.menu-body');
     
-    if (iconMenu) {
-        iconMenu.addEventListener("click", function (e) {
-            document.body.classList.toggle('lock');
-            iconMenu.classList.toggle("open-menu");
-            menuBody.classList.toggle("open-menu");
-        });
+//     if (iconMenu) {
+//         iconMenu.addEventListener("click", function (e) {
+//             document.body.classList.toggle('lock');
+//             iconMenu.classList.toggle("open-menu");
+//             menuBody.classList.toggle("open-menu");
+//         });
     
-    }
-    
-    // if (homeMenuLinks.length > 0) {
-    //     homeMenuLinks.forEach(link => {
-    //         link.onclick = function (event) {
-    //             event.preventDefault();
-    //             onMenuLinkClick(this.getAttribute("href"));
-    //         }
-    //     });
-    
-    //     function onMenuLinkClick(href) {
-    //         if (document.querySelector(href)) {
-    //             const target = document.querySelector(href);
-    //             let targetLocation = target.getBoundingClientRect().top + window.pageYOffset;
-    
-    //             if (iconMenu.classList.contains("open-menu")) {
-    //                 document.body.classList.remove('lock');
-    //                 iconMenu.classList.remove("open-menu");
-    //                 menuBody.classList.remove("open-menu");
-    //             }
-    //             window.scrollTo({
-    //                 top: targetLocation,
-    //                 behavior: "smooth"
-    //             });
-    //         }
-    //     }
-    // }
-    
-}
-
-// function createScrollListener(elementSelector, visibleCallback, hiddenCallback, threshold) {
-//     const element = elementSelector;
-
-//     function handleIntersection(entries) {
-//         if (entries[0].isIntersecting) {
-//             visibleCallback();
-//         } else {
-//             hiddenCallback();
-//         }
 //     }
-
-//     const options = {
-//         threshold: threshold || 0.5
-//     };
-
-//     const observer = new IntersectionObserver(handleIntersection, options);
-//     observer.observe(element);
+    
+//     // if (homeMenuLinks.length > 0) {
+//     //     homeMenuLinks.forEach(link => {
+//     //         link.onclick = function (event) {
+//     //             event.preventDefault();
+//     //             onMenuLinkClick(this.getAttribute("href"));
+//     //         }
+//     //     });
+    
+//     //     function onMenuLinkClick(href) {
+//     //         if (document.querySelector(href)) {
+//     //             const target = document.querySelector(href);
+//     //             let targetLocation = target.getBoundingClientRect().top + window.pageYOffset;
+    
+//     //             if (iconMenu.classList.contains("open-menu")) {
+//     //                 document.body.classList.remove('lock');
+//     //                 iconMenu.classList.remove("open-menu");
+//     //                 menuBody.classList.remove("open-menu");
+//     //             }
+//     //             window.scrollTo({
+//     //                 top: targetLocation,
+//     //                 behavior: "smooth"
+//     //             });
+//     //         }
+//     //     }
+//     // }
+    
 // }
 
+// // function createScrollListener(elementSelector, visibleCallback, hiddenCallback, threshold) {
+// //     const element = elementSelector;
+
+// //     function handleIntersection(entries) {
+// //         if (entries[0].isIntersecting) {
+// //             visibleCallback();
+// //         } else {
+// //             hiddenCallback();
+// //         }
+// //     }
+
+// //     const options = {
+// //         threshold: threshold || 0.5
+// //     };
+
+// //     const observer = new IntersectionObserver(handleIntersection, options);
+// //     observer.observe(element);
+// // }
+
+const header = document.querySelector('.header');
+const iconMenu = document.querySelector('.menu-icon');
+const homeMenuLinks = document.querySelectorAll(".scroll-to");
+const menuBody = document.querySelector('.menu-body');
+const homePage = document.querySelector(".main");
+
+
+let lastScrollTop = 0;
+
+function navMenuBackground() {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        header.classList.add("header-hidden");
+    } else {
+        header.classList.remove("header-hidden");
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+
+}
+
+window.addEventListener(`scroll`, navMenuBackground);
+
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('lock');
+        iconMenu.classList.toggle("open-menu");
+        menuBody.classList.toggle("open-menu");
+    });
+
+}
+
+if (homeMenuLinks.length > 0) {
+    homeMenuLinks.forEach(link => {
+        link.onclick = function (event) {
+            event.preventDefault();
+            onMenuLinkClick(this.getAttribute("href"));
+        }
+    });
+
+    function onMenuLinkClick(href) {
+        if (document.querySelector(href)) {
+            const target = document.querySelector(href);
+            let targetLocation = target.getBoundingClientRect().top + window.pageYOffset;
+
+            if (!header.classList.contains("header-hidden") && targetLocation < window.pageYOffset) {
+                targetLocation = target.getBoundingClientRect().top + window.pageYOffset - header.offsetHeight;
+            }
+
+            if (iconMenu.classList.contains("open-menu")) {
+                document.body.classList.remove('lock');
+                iconMenu.classList.remove("open-menu");
+                menuBody.classList.remove("open-menu");
+            }
+            // scrollToBlock(href);
+            window.scrollTo({
+                top: targetLocation,
+                behavior: "smooth"
+            });
+        }
+    }
+}
